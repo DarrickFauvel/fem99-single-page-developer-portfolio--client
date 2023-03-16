11beb349279293ae0fd1ee09e723f8a931e9abca
Darrick Fauvel <darrick.fauvel@gmail.com>
2023-03-15 20:24:19 -0400

    Add contact section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    host: "0.0.0.0",
+    host: "0.0.0.0"
   },
-})
+  integrations: [react()]
+});
---
+++ src/data/index.jsx
@@ -0,0 +1,174 @@
+import iconGithub from "../assets/images/icon-github.svg"
+import iconFrontendMentor from "../assets/images/icon-frontend-mentor.svg"
+import iconLinkedin from "../assets/images/icon-linkedin.svg"
+import iconTwitter from "../assets/images/icon-twitter.svg"
+
+export const siteInfo = {
+  brand: "adamkeyes",
+  author: "Adam Keyes",
+}
+
+export const socials = [
+  {
+    name: "GitHub",
+    url: "javascript:void(0)",
+    image: "icon-github.svg",
+    alt: "GitHub profile",
+  },
+  {
+    name: "Frontend Mentor",
+    url: "javascript:void(0)",
+    image: "icon-frontend-mentor.svg",
+    alt: "Frontend Mentor profile",
+  },
+  {
+    name: "LinkedIn",
+    url: "javascript:void(0)",
+    image: "icon-linkedin.svg",
+    alt: "LinkedIn profile",
+  },
+  {
+    name: "Twitter",
+    url: "javascript:void(0)",
+    image: "icon-twitter.svg",
+    alt: "Twitter profile",
+  },
+]
+
+export const skills = [
+  {
+    name: "HTML",
+    description: "4 Years Experience",
+  },
+  {
+    name: "CSS",
+    description: "4 Years Experience",
+  },
+  {
+    name: "JavaScript",
+    description: "4 Years Experience",
+  },
+  {
+    name: "Accessibility",
+    description: "4 Years Experience",
+  },
+  {
+    name: "React",
+    description: "3 Years Experience",
+  },
+  {
+    name: "Sass",
+    description: "3 Years Experience",
+  },
+]
+
+export const projects = [
+  {
+    title: "Design Portfolio",
+    stack: ["html", "css"],
+    image: {
+      small: "thumbnail-project-1-small.webp",
+      large: "thumbnail-project-1-large.webp",
+    },
+    links: [
+      {
+        title: "View Project",
+        url: "javascript:void(0)",
+      },
+      {
+        title: "View Code",
+        url: "javascript:void(0)",
+      },
+    ],
+  },
+  {
+    title: "E-Learning Landing Page",
+    stack: ["html", "css"],
+    image: {
+      small: "thumbnail-project-2-small.webp",
+      large: "thumbnail-project-2-large.webp",
+    },
+    links: [
+      {
+        title: "View Project",
+        url: "javascript:void(0)",
+      },
+      {
+        title: "View Code",
+        url: "javascript:void(0)",
+      },
+    ],
+  },
+  {
+    title: "Todo Web App",
+    stack: ["html", "css", "javascript"],
+    image: {
+      small: "thumbnail-project-3-small.webp",
+      large: "thumbnail-project-3-large.webp",
+    },
+    links: [
+      {
+        title: "View Project",
+        url: "javascript:void(0)",
+      },
+      {
+        title: "View Code",
+        url: "javascript:void(0)",
+      },
+    ],
+  },
+  {
+    title: "Entertainment Web App",
+    stack: ["html", "css", "javascript"],
+    image: {
+      small: "thumbnail-project-4-small.webp",
+      large: "thumbnail-project-4-large.webp",
+    },
+    links: [
+      {
+        title: "View Project",
+        url: "javascript:void(0)",
+      },
+      {
+        title: "View Code",
+        url: "javascript:void(0)",
+      },
+    ],
+  },
+  {
+    title: "Memory Game",
+    stack: ["html", "css", "javascript"],
+    image: {
+      small: "thumbnail-project-5-small.webp",
+      large: "thumbnail-project-5-large.webp",
+    },
+    links: [
+      {
+        title: "View Project",
+        url: "javascript:void(0)",
+      },
+      {
+        title: "View Code",
+        url: "javascript:void(0)",
+      },
+    ],
+  },
+  {
+    title: "Art Gallery Showcase",
+    stack: ["html", "css", "javascript"],
+    image: {
+      small: "thumbnail-project-6-small.webp",
+      large: "thumbnail-project-6-large.webp",
+    },
+    links: [
+      {
+        title: "View Project",
+        url: "javascript:void(0)",
+      },
+      {
+        title: "View Code",
+        url: "javascript:void(0)",
+      },
+    ],
+  },
+]
